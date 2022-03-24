2b106e71f9abcdcde0682756b53d8fb414f5f54c
zn0228 <zn0228@naver.com>
2022-03-24 23:39:02 +0900

    TestJS

--- todolist.js
+++ todolist.js
@@ -1,2 +1,25 @@
+const button = document.querySelector("#button");
 
+const push_button = document.querySelector("#button");
+push_button.addEventListener("click", () => {
+  const input_box = document.querySelector("#input_in");
+  const text = input_box.value();
 
+  if (text !== "") {
+    add_list(text);
+    input_box.value = "";
+    input_box.focus();
+  }
+});
+
+// add_list.addEventListener("click",
+function add_list(text) {
+  const list = document.createElement("#list");
+
+  const new_list = document.createElement("li");
+  new_list.classList.add("list_item");
+
+  new_list.innerHTML = text;
+
+  list.appendChild(new_list);
+}
