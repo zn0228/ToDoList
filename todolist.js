c9c5868c62664aa1728509c85c6aa4bc5be5bea9
zn0228 <zn0228@naver.com>
2022-04-03 22:37:16 +0900

    ChangeDelete

--- todolist.js
+++ todolist.js
@@ -1,7 +1,6 @@
 const list_box = document.getElementById("list");
 const input_box = document.getElementById("input_box");
 const input_in = input_box.querySelector("#input_in");
-const delete_button = document.querySelector(".delete_button");
 
 const put = (event) => {
   event.preventDefault();
@@ -19,13 +18,13 @@ const addlist = (text) => {
   newDeleteButton.className = "delete_button";
   newDeleteButton.innerText = "X";
   newlist.appendChild(newDeleteButton);
+  // const delete_button = newDeleteButton.querySelector(".delete_button");
+  newDeleteButton.addEventListener("click", delete_list);
 };
 
 const delete_list = (event) => {
   const removing_one = event.target.parentElement;
   removing_one.remove();
 };
-
-delete_button.addEventListener("click", delete_list);
 
 input_box.addEventListener("submit", put);
