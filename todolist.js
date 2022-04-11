ba15c7a89e178d2977fce9e604080e5bf14fd742
zn0228 <zn0228@naver.com>
2022-04-11 23:13:05 +0900

    addArray

--- todolist.js
+++ todolist.js
@@ -2,31 +2,39 @@ const list_box = document.getElementById("list");
 const input_box = document.getElementById("input_box");
 const input_in = input_box.querySelector("#input_in");
 
+const toDo = [];
 const put = (event) => {
   event.preventDefault();
-  addlist(input_in.value);
+  toDp.push(input_in.value);
   input_in.value = "";
 };
 
-const addlist = (text) => {
-  if (text != "") {
-    const newlist = document.createElement("li");
-    newlist.className = "list_item";
-    newlist.innerText = text;
-    list_box.appendChild(newlist);
-    const newDeleteButton = document.createElement("button");
+const show = () => {};
+//   const put = (event) => {
+//     event.preventDefault();
+//     addlist(input_in.value);
+//     input_in.value = "";
+//   };
 
-    newDeleteButton.className = "delete_button";
-    newDeleteButton.innerText = "X";
-    newlist.appendChild(newDeleteButton);
-    // const delete_button = newDeleteButton.querySelector(".delete_button");
-    newDeleteButton.addEventListener("click", delete_list);
-  }
-};
+// const addlist = (text) => {
+//   if (text != "") {
+//     const newlist = document.createElement("li");
+//     newlist.className = "list_item";
+//     newlist.innerText = text;
+//     list_box.appendChild(newlist);
+//     const newDeleteButton = document.createElement("button");
 
-const delete_list = (event) => {
-  const removing_one = event.target.parentElement;
-  removing_one.remove();
-};
+//     newDeleteButton.className = "delete_button";
+//     newDeleteButton.innerText = "X";
+//     newlist.appendChild(newDeleteButton);
+//     // const delete_button = newDeleteButton.querySelector(".delete_button");
+//     newDeleteButton.addEventListener("click", delete_list);
+//   }
+// };
+
+// const delete_list = (event) => {
+//   const removing_one = event.target.parentElement;
+//   removing_one.remove();
+// };
 
 input_box.addEventListener("submit", put);
