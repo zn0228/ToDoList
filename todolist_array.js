7b7b104b246e96fa7202adb53fbc5982e45be4e3
zn0228 <zn0228@naver.com>
2022-04-19 19:11:56 +0900

    sliceJS

--- todolist_array.js
+++ todolist_array.js
@@ -2,31 +2,41 @@ const list_box = document.getElementById("list");
 const input_box = document.getElementById("input_box");
 const input_in = input_box.querySelector("#input_in");
 
+const toDo = [];
 const put = (event) => {
   event.preventDefault();
-  addlist(input_in.value);
-  input_in.value = "";
+  if (input_in.value != "") {
+    toDo.push(input_in.value);
+    input_in.value = "";
+    addlist();
+  }
 };
 
-const addlist = (text) => {
-  if (text != "") {
+const addlist = () => {
+  list_box.innerHTML = "";
+  toDo.forEach((text, index) => {
     const newlist = document.createElement("li");
     newlist.className = "list_item";
     newlist.innerText = text;
     list_box.appendChild(newlist);
-    const newDeleteButton = document.createElement("button");
 
+    const newDeleteButton = document.createElement("button");
     newDeleteButton.className = "delete_button";
     newDeleteButton.innerText = "X";
     newlist.appendChild(newDeleteButton);
-    // const delete_button = newDeleteButton.querySelector(".delete_button");
-    newDeleteButton.addEventListener("click", delete_list);
-  }
+    newDeleteButton.addEventListener("click", () => delete_list(index));
+  });
 };
 
-const delete_list = (event) => {
-  const removing_one = event.target.parentElement;
-  removing_one.remove();
+delete_list = (index) => {
+  const array = toDo.filter((todo) => {
+    todo !== toDo[index];
+  });
+  console.log(array);
+  toDo = array;
+
+  // toDo.splice(index, 1);
+  addlist();
 };
 
 input_box.addEventListener("submit", put);
